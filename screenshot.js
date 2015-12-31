c7ac348421f5b1b25c1cef775d3995fe6e242c15
abram stern <aphid@ucsc.edu>
2015-12-30 18:08:22 -0800

    add screenshot

---
+++ screenshot.js
@@ -0,0 +1,20 @@
+var url = phantom.args[0];
+var dir = "/var/www/html/webshots/";
+var filename = phantom.args[1];
+
+var target = dir + filename + ".png";
+
+console.log("writing to " + target);
+
+var page = require('webpage').create();
+page.settings.userAgent = 'Mozilla / 5.0(compatible; MSIE 10.0; Windows NT 6.1; Trident / 6.0';
+page.viewportSize = {
+  width: 1270,
+  height: 1116
+};
+page.open(url, function () { // executed after 
+  page.render(dir + filename + ".png");
+
+}).then(function () {
+  slimer.exit();
+});
