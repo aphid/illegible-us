08f41b3976ca0c4ef6198aa3cb55d17f47c6328b
abram stern <aphid@ucsc.edu>
2016-01-04 20:20:29 -0800

    forgot a quote

--- app.js
+++ app.js
@@ -1223,7 +1223,7 @@ Committee.prototype.getHearingIndex = function (url) {
         socksPort: 9050
       },
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36'
       }
     };
     scraper.msg("trying " + JSON.stringify(options));
